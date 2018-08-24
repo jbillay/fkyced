98463915f2c25416e9d414813d58a1784460562b
Jeremy Billay <jbilla@gmail.com>
2018-08-24 16:11:51 +0100

    Add always required management and section navigation

--- admin/public/javascripts/fkycedBuildForm.js
+++ admin/public/javascripts/fkycedBuildForm.js
@@ -1,6 +1,5 @@
 // fkycedBuildForm Javascript
 /*
-TODO: Slider between fields / components
 TODO: JSON export function
 TODO: Display library
 */
@@ -20,33 +19,37 @@ class fkycedBuildForm {
     $('div#fkycedFormBuilderCenterPanel').droppable({
       accept: '#fkycedSelectableItems li',
       drop: function( event, ui ) {
-        const itemName = ui.draggable.attr("name")
+        const itemId = parseInt(ui.draggable.attr('id'))
         let itemType = null
         let attributes = {}
         if (datas.objects) {
           for (const [objectKey, objectInfo] of Object.entries(datas.objects)) {
             for (const [key, value] of Object.entries(objectInfo.fields)) {
-              if (value.name === itemName) {
+              if (parseInt(value.id) === itemId) {
+                attributes['id'] = value.id
                 attributes['name'] = value.name
                 attributes['label'] = value.label
+                attributes['alwaysRequired'] = value.required
                 itemType = value.fieldType
               }
             }
           }
-        }
-        if (itemType) {
-          const functionName = 'add' + itemType.replace(/^\w/, c => c.toUpperCase())
-          console.log(functionName)
-          const { newElement, panelId } = $(BuildItemBuilder[functionName](attributes))[0]
-          $('div#fkycedFormBuilderCenterPanel').append(newElement)
-          const closeIconSelector = `div#${panelId} i.fa-window-close`
-          const editIconSelector = `div#${panelId} i.fa-edit`
-          const requriedCheckboxSelector = `#required_${panelId}`
-          const optionPanelSelector = `div#option_${panelId}`
-          $(optionPanelSelector).hide()
-          $(closeIconSelector).click(function (event) { BuildItemBuilder.removePanel($(this).parent().parent().attr('id'))  })
-          $(editIconSelector).click(function (event) { BuildItemBuilder.toggleOptions($(this).parent().parent().attr('id'))  })
-          $(requriedCheckboxSelector).click(function (event) { BuildItemBuilder.toggleRequired($(this).parent().parent().parent().attr('id'))  })
+          if (itemType) {
+            const functionName = 'add' + itemType.replace(/^\w/, c => c.toUpperCase())
+            const { newElement, panelId } = $(BuildItemBuilder[functionName](attributes))[0]
+            $('div#fkycedFormBuilderCenterPanel').append(newElement)
+            if (attributes.alwaysRequired) {
+              BuildItemBuilder.toggleRequired(panelId)
+            }
+            const closeIconSelector = `div#${panelId} i.fa-window-close`
+            const editIconSelector = `div#${panelId} i.fa-edit`
+            const requiredCheckboxSelector = `#required_${panelId}`
+            const optionPanelSelector = `div#option_${panelId}`
+            $(optionPanelSelector).hide()
+            $(closeIconSelector).click(function (event) { BuildItemBuilder.removePanel($(this).parent().parent().attr('id'))  })
+            $(editIconSelector).click(function (event) { BuildItemBuilder.toggleOptions($(this).parent().parent().attr('id'))  })
+            $(requiredCheckboxSelector).click(function (event) { BuildItemBuilder.toggleRequired($(this).parent().parent().parent().attr('id'))  })
+          }
         }
       }
     })
@@ -59,31 +62,81 @@ class fkycedBuildForm {
         label: 'Fields',
         name: 'fields',
         visible: true },
-        {
-          label: 'Components',
-          name: 'components',
-          visible: false }
-        ]
+      {
+        label: 'Components',
+        name: 'components',
+        visible: false }
+    ]
+    const obj = this
     const itemsSection = `<div id="ItemsSection"><div id="itemTypeSelector"></div></div><div id="actionSection"></div>`
     element.append(itemsSection)
     $('div#itemTypeSelector').append(this.createSectionSelector(sections))
-    $('a.previousSection').click(function () {
-      alert('previousSection')
+    sections.forEach(function (section) {
+      const functionName = 'create' + section.name.replace(/^\w/, c => c.toUpperCase()) + 'List'
+      obj[functionName]($('div#ItemsSection'), datas, section.visible)
     })
-    $('a.nextSection').click(function () {
-      let next = 0
+    $('a.previousSection').click(function () {
+      let sectionTable = []
       $('div.sectionHeader').each(function (index) {
-          if (next === 1) {
-            $(this).removeClass('SectionNotDisplayed')
-            next = 0
-          } else if (!$(this).hasClass('SectionNotDisplayed')) {
-            console.log('ICI')
-            $(this).addClass('SectionNotDisplayed')
-            next = 1
-          }
+        sectionTable[index] = !$(this).hasClass('SectionNotDisplayed')
+      })
+      let currentIndex = null
+      let newIndex = null
+      let maxIndex = 0
+      sectionTable.forEach(function (value, index) {
+        if (value) {
+          currentIndex = index
+        }
+        maxIndex = index
+      })
+      if (currentIndex === 0) {
+        newIndex = maxIndex
+      } else {
+        newIndex = currentIndex - 1
+      }
+      $('div.sectionHeader').each(function (index) {
+        if (index === currentIndex) {
+          $(this).addClass('SectionNotDisplayed')
+          const currentListPanelSelector = $(this).attr('id').substring(8).replace(/^\w/, c => c.toUpperCase()) + 'List'
+          $('div#' + currentListPanelSelector).addClass('SectionNotDisplayed')
+        } else if (index == newIndex) {
+          $(this).removeClass('SectionNotDisplayed')
+          const currentListPanelSelector = $(this).attr('id').substring(8).replace(/^\w/, c => c.toUpperCase()) + 'List'
+          $('div#' + currentListPanelSelector).removeClass('SectionNotDisplayed')
+        }
       })
     })
-    this.createFieldsList($('div#ItemsSection'), datas, true)
+    $('a.nextSection').click(function () {
+      let sectionTable = []
+      $('div.sectionHeader').each(function (index) {
+        sectionTable[index] = !$(this).hasClass('SectionNotDisplayed')
+      })
+      let currentIndex = null
+      let newIndex = null
+      let maxIndex = 0
+      sectionTable.forEach(function (value, index) {
+        if (value) {
+          currentIndex = index
+        }
+        maxIndex = index
+      })
+      if (currentIndex === maxIndex) {
+        newIndex = 0
+      } else {
+        newIndex = currentIndex + 1
+      }
+      $('div.sectionHeader').each(function (index) {
+        if (index === currentIndex) {
+          $(this).addClass('SectionNotDisplayed')
+          const currentListPanelSelector = $(this).attr('id').substring(8).replace(/^\w/, c => c.toUpperCase()) + 'List'
+          $('div#' + currentListPanelSelector).addClass('SectionNotDisplayed')
+        } else if (index == newIndex) {
+          $(this).removeClass('SectionNotDisplayed')
+          const currentListPanelSelector = $(this).attr('id').substring(8).replace(/^\w/, c => c.toUpperCase()) + 'List'
+          $('div#' + currentListPanelSelector).removeClass('SectionNotDisplayed')
+        }
+      })
+    })
   }
 
   createSectionSelector (sections) {
@@ -98,24 +151,28 @@ class fkycedBuildForm {
     return html
   }
 
-  createComponentsList (element, visible) {
+  createComponentsList (element, datas, visible) {
+    let html = '<div id="ComponentsList"'
+    html += visible ? '' : ' class="SectionNotDisplayed"'
+    html += '><ol id="fkycedSelectableItems"></ol></div>'
+    element.append(html)
   }
 
   createFieldsList (element, datas, visible) {
-    let htmlList = `<div id="itemsList">
-      <input type="text" class="form-control" id="ListItemsFilter" name="ListItemsFilter" placeholder="Enter field name">
+    let htmlList = `<div id="FieldsList">
+      <input type="text" class="form-control" id="ListFieldsFilter" name="ListFieldsFilter" placeholder="Enter field name">
       <ol id="fkycedSelectableItems">`
     if (datas.objects) {
       for (const [objectKey, objectInfo] of Object.entries(datas.objects)) {
         for (const [key, value] of Object.entries(objectInfo.fields)) {
-          htmlList += `<li class="ui-widget-content" name="${value.name}">${objectInfo.name}.${value.label}</li>`
+          htmlList += `<li class="ui-widget-content" name="${value.name}" id="${value.id}">${objectInfo.name}.${value.label}</li>`
         }
       }
     }
     htmlList += '</ol></div>'
     element.append(htmlList)
     $('#fkycedSelectableItems li').draggable({ revert: true })
-    $('#ListItemsFilter').bind('input', function (event) {
+    $('#ListFieldsFilter').bind('input', function (event) {
       const filter = $(this).val()
       $('#fkycedSelectableItems li').each(function(index) {
         const pattern = new RegExp(filter, "ig")
@@ -127,6 +184,10 @@ class fkycedBuildForm {
       });
     })
   }
+
+  exportJson () {
+
+  }
 }
 
 class BuildItem {
@@ -134,11 +195,9 @@ class BuildItem {
     const item = `<div class="form-group">
     <label class="control-label" for="${attributes.name}">${attributes.label}</label>
     <select class="form-control" id="${attributes.name}" name="${attributes.name}">
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
-    <option>4</option>
-    <option>5</option>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
     </select>
     </div>`
     return item
@@ -209,7 +268,7 @@ class BuildItemBuilder extends BuildItem {
     }
   }
 
-  static toggleRequired (id ) {
+  static toggleRequired (id) {
     const panelSelector = `div#${id}`
     const labelItem = $(panelSelector).children('.form-group')
     if (labelItem.hasClass('required')) {
@@ -219,48 +278,50 @@ class BuildItemBuilder extends BuildItem {
     }
   }
 
-  static addConfigPanel (element) {
+  static addConfigPanel (element, alwaysRequired) {
     const panelId = 'panel_' + Math.random().toString(36).substr(2, 17)
-    const newElement = `<div class="configPanel" id="${panelId}">
-    <div class="itemIcons float-right"><i class="far fa-edit"></i><i class="far fa-window-close"></i></div>
-    ${element}
-    <div class="optionPanel" id="option_${panelId}">
-    <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="required_${panelId}">
-    <label class="form-check-label" for="required_${panelId}">Required</label>
-    </div>
-    </div>
-    </div>`
+    let newElement = `<div class="configPanel" id="${panelId}">
+      <div class="itemIcons float-right"><i class="far fa-edit"></i><i class="far fa-window-close"></i></div>
+      ${element}
+      <div class="optionPanel" id="option_${panelId}">
+      <div class="form-group form-check">`
+    if (alwaysRequired) {
+      const readOnlyChecked = 'disabled checked="checked"'
+      newElement += `<input type="checkbox" class="form-check-input" id="required_${panelId}" ${readOnlyChecked}>`
+    } else {
+      newElement += `<input type="checkbox" class="form-check-input" id="required_${panelId}">`
+    }
+    newElement += `<label class="form-check-label" for="required_${panelId}">Required</label></div></div></div>`
     return { newElement: newElement, panelId: panelId }
   }
 
   static addPicklist (attributes) {
     let newItem = super.addPicklist(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
   static addCheckbox (attributes) {
     let newItem = super.addCheckbox(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
   static addText (attributes) {
     let newItem = super.addText(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
   static addPhone (attributes) {
     let newItem = super.addPhone(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
   static addNumber (attributes) {
     let newItem = super.addNumber(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
   static addCurrency (attributes) {
     let newItem = super.addCurrency(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
   static addDate (attributes) {
     let newItem = super.addDate(attributes)
-    return this.addConfigPanel(newItem)
+    return this.addConfigPanel(newItem, attributes.alwaysRequired)
   }
 }
 
